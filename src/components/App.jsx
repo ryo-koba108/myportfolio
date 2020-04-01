c0bff7bf619648afbf244b1e97e59d83e52b81c4
ryota kobayashi <ryotakobayashi@koba.local>
2020-04-02 01:19:04 +0900

    header component end

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  HashRouter as Router,
+  Route
+} from 'react-router-dom'
+import Home from './Home';
+import Header from './Header';
+import Work from './Work';
+import Contact from './Contact';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/work" exact component={Work}/>
+        <Route path="/contact" exact component={Contact} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Work.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+class Work extends React.Component {
+  render(){
+    return(
+      <h1>work</h1>
+    )
+  }
+}
+
+
+export default Work;
