19d31d149ceffa6811f693d49febb9a7ce1ce188
ryota kobayashi <ryotakobayashi@koba.local>
2020-04-02 19:20:01 +0900

    Updates

---
+++ precache-manifest.329c3ea66c4065e4de565365d74141c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b403b26547bb2731c561ca92ad830b49",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "322e81750e3a20734710",
+    "url": "/myportfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "322e81750e3a20734710",
+    "url": "/myportfolio/static/js/2.d0d1633f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myportfolio/static/js/2.d0d1633f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a781f6b754b452ce045",
+    "url": "/myportfolio/static/js/main.18592fec.chunk.js"
+  },
+  {
+    "revision": "3bd7f5d553c0c6c5ad7a",
+    "url": "/myportfolio/static/js/runtime-main.226198fe.js"
+  },
+  {
+    "revision": "30e1b05b4f853512816099db13f18a42",
+    "url": "/myportfolio/static/media/koba-img.30e1b05b.jpg"
+  }
+]);
